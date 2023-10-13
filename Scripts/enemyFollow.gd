c2c4fc6109332142f93c9f75c6a42ed7e23f98ec
Elemental Paladin <58434079+Shadow-Diamond@users.noreply.github.com>
2023-10-13 17:18:01 -0500

    Added Enemy With Movement

--- Scripts/enemyFollow.gd
+++
@@ -1,7 +0,0 @@
-extends PathFollow2D
-
-export var moveSpeed = 20
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	set_offset(get_offset() + moveSpeed * delta)
--- Scripts/enemyMovement.gd
+++ Scripts/enemyMovement.gd
@@ -1,11 +1,14 @@
-extends Path2D
+extends PathFollow2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	progress_ratio = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var speed: float = 0.15
+	progress_ratio += delta * speed
+	if(progress_ratio > 1.0):
+		progress_ratio = 0.0
