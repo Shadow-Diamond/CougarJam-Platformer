440fd1e43d0f51668b5b55500b66b60536dba83e
Cameron Moss <125421489+CameronMoss991@users.noreply.github.com>
2023-10-14 08:06:41 -0500

    junk

--- Scenes/Enemies/BatArea2D.gd
+++ Scenes/Enemies/BatArea2D.gd
@@ -1,15 +1,15 @@
-extends PathFollow2D
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	progress_ratio = 0
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var speed: float = 0.08
-	progress_ratio += delta * speed
-	if(progress_ratio > 1.0):
-		progress_ratio = 0.0
+	pass
 
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		body.gameover()
--- Scenes/Enemies/CharacterBody2D.gd
+++ Scenes/Enemies/CharacterBody2D.gd
@@ -1,15 +1,11 @@
-extends PathFollow2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	progress_ratio = 0
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var speed: float = 0.08
-	progress_ratio += delta * speed
-	if(progress_ratio > 1.0):
-		progress_ratio = 0.0
-
+	pass
