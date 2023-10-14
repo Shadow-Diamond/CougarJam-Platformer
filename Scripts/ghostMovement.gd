e5ea7ad23c385cf3f95b1c7e3c2ae4de932dc4bc
Elemental Paladin <58434079+Shadow-Diamond@users.noreply.github.com>
2023-10-13 22:34:19 -0500

    Added Ghost, Renamed Stuff

---
+++ Scripts/ghostMovement.gd
@@ -0,0 +1,14 @@
+extends PathFollow2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	progress_ratio = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var speed: float = 0.2
+	progress_ratio += delta * speed
+	if(progress_ratio > 1.0):
+		progress_ratio = 0.0
